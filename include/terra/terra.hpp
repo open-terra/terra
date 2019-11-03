930a25dc586c829af06a258330d5f54be09790bf
c0rp3n <ojhitchcock@gmail.com>
2019-11-03 15:34:56 +0000

    Add terra.hpp
    Includes all components of Terra except for utils

---
+++ include/terra/terra.hpp
@@ -0,0 +1,12 @@
+#pragma once
+
+#include "noise/noise.hpp"
+
+#include "types/hash_grid.hpp"
+#include "types/rect.hpp"
+#include "types/vector_2.hpp"
+#include "types/vector_3.hpp"
+
+#include "delaunator.hpp"
+#include "poisson_disc_sampler.hpp"
+#include "voronoi.hpp"
