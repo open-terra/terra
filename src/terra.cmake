20cb944c748e1a1370b6a815656658d266316f1a
c0rp3n <ojhitchcock@gmail.com>
2019-11-03 15:44:00 +0000

    Correct terra.cmake

--- src/terra.cmake
+++ src/terra.cmake
@@ -1,8 +1,8 @@
 add_library(terra STATIC
-    src/grid/poisson_disc_sampler.cpp
     src/noise/open_simplex.cpp
     src/noise/summed_noise.cpp
     src/types/hash_grid.cpp
     src/delaunator.cpp
+    src/poisson_disc_sampler.cpp
     src/voronoi.cpp
 )
