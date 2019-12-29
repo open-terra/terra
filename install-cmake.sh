f7279d4e41f3b2d2f4578b6b80ec2706f22a5881
c0rp3n <ojhitchcock@gmail.com>
2019-12-29 14:26:56 +0000

    fix?

--- install-cmake.sh
+++
@@ -1,3 +0,0 @@
-CMAKE_URL="https://cmake.org/files/v3.16/cmake-3.16.2-Linux-x86_64.tar.gz"
-mkdir cmake && travis_retry wget --no-check-certificate --quiet -O - ${CMAKE_URL} | tar --strip-components=1 -xz -C cmake
-export PATH=${DEPS_DIR}/cmake/bin:${PATH}
