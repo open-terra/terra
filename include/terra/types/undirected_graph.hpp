e1dbd5580aca2cd72838efcebef26cae43fea9d8
c0rp3n <ojhitchcock@gmail.com>
2019-12-29 20:18:12 +0000

    [Undirectional Graph] Fix usage

--- include/terra/types/undirected_graph.hpp
+++ include/terra/types/undirected_graph.hpp
@@ -2,9 +2,17 @@
 
 #include <cstdint>
 
-#include <boost/graph/adjacency_list.hpp>
-
 namespace terra
 {
-    typedef boost::adjacency_list<boost::vecS, boost::vecS, boost::undirectedS, size_t, size_t> undirected_graph;
+    typedef boost::property<boost::vertex_index_t, size_t> vertex_property;
+    typedef boost::property<boost::edge_index_t, std::size_t> edge_property;
+
+    typedef boost::adjacency_list<boost::setS,
+                                  boost::vecS,
+                                  boost::undirectedS,
+                                  vertex_property,
+                                  edge_property
+                                  >undirected_graph;
+
+    typedef boost::graph_traits<undirected_graph> undirected_graph_traits;
 }
