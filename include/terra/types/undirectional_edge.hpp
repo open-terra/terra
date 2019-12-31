e1a536930d1d22bc0b3ed2ec039ee9bfbc9dd8a0
c0rp3n <ojhitchcock@gmail.com>
2019-12-31 16:53:36 +0000

    [Undirectional Edge] Move source to header for constexpr

--- include/terra/types/undirectional_edge.hpp
+++ include/terra/types/undirectional_edge.hpp
@@ -7,13 +7,39 @@ namespace terra
 {
     struct undirectional_edge
     {
+    public:
         size_t v0, v1;
 
-        constexpr undirectional_edge();
-        constexpr undirectional_edge(size_t v0, size_t v1);
+        constexpr undirectional_edge() : v0(0), v1(0)
+        {
+        }
 
-        constexpr std::array<size_t, 2ul> get_vertex() const;
+        constexpr undirectional_edge(size_t v0, size_t v1) : v0(v0), v1(v1)
+        {
+        }
+
+        ~undirectional_edge()
+        {
+        }
+
+        constexpr std::array<size_t, 2ul> get_vertex() const
+        {
+            return { this->v0, this->v1 };
+        }
+
+        constexpr void sort_vertex()
+        {
+            if (this->v0 > this->v1)
+            {
+                std::swap(this->v0, this->v1);
+            }
+        }
     };
 }
 
-constexpr bool operator==(const terra::undirectional_edge&, const terra::undirectional_edge&);
+constexpr bool operator==(const terra::undirectional_edge& l,
+                          const terra::undirectional_edge& r)
+{
+    return (l.v0 == r.v0 && l.v1 == r.v1) ||
+           (l.v0 == r.v1 && l.v1 == r.v0);
+}
