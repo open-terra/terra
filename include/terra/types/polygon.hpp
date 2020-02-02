5e7746fe075ca508fa122386f0a3849b486babe9
c0rp3n <ojhitchcock@gmail.com>
2020-02-02 17:37:33 +0000

    [Polygon] Add area method

--- include/terra/types/polygon.hpp
+++ include/terra/types/polygon.hpp
@@ -19,5 +19,31 @@ namespace terra
             centre_idx(centre_idx), vertices(vertices)
         {
         }
+
+        double area(const terra::vec2& centre) const
+        {
+            double area = area_of_tri(centre,
+                                      this->vertices.front(),
+                                      this->vertices.back());
+            for (size_t i = 1; i < this->vertices.size(); ++i)
+            {
+                const auto& v0 = this->vertices[i - 1];
+                const auto& v1 = this->vertices[i];
+
+                area += area_of_tri(centre, v0, v1);
+            }
+
+            return area;
+        }
+
+    private:
+        double area_of_tri(const terra::vec2& a, const terra::vec2& b, const terra::vec2& c) const
+        {
+            double area2 = (a.x * (b.y - c.y)) +
+                           (b.x * (c.y - a.y)) +
+                           (c.x * (a.y - b.y));
+
+            return area2 / 2;
+        }
     };
 }
