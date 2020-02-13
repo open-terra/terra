61ec0c90feeb7c04978c2a82211dad1de88206ae
c0rp3n <ojhitchcock@gmail.com>
2020-02-13 15:51:23 +0000

    [CI] Fix appveyor build

--- scripts/install-fastnoise-simd.bat
+++ scripts/install-fastnoise-simd.bat
@@ -1,5 +1,5 @@
 git clone https://github.com/c0rp3n/fastnoise-simd.git
 mkdir fastnoise-simd/build
 cd fastnoise-simd/build
-cmake -DBUILD_TESTING=OFF -DCMAKE_INSTALL_PREFIX="../../deps/FastNoiseSIMD" ..
+cmake -DFN_COMPILE_AVX2=ON -DFN_COMPILE_AVX512=OFF -DBUILD_TESTING=OFF -DCMAKE_INSTALL_PREFIX="../../deps/FastNoiseSIMD" ..
 cmake --build . --target install
