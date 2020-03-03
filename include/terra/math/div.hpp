2277149633f9d7576858457b5875535d2d6d3b0a
c0rp3n <ojhitchcock@gmail.com>
2020-03-03 12:20:14 +0000

    [Math] Add div

---
+++ include/terra/math/div.hpp
@@ -0,0 +1,19 @@
+#pragma once
+
+namespace terra::math
+{
+    template<typename T>
+    struct div_t
+    {
+        T quot
+        T rem;
+    }
+
+    template<typename T = size_t, typename U = size_t>
+    constexpr div_t<U> div(T x, T y)
+    {
+        const U quot = x / y;
+        const U rem = x % y;
+        return { quot, rem };
+    }
+} // namespace terra::math
