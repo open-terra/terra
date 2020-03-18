2e50449542ddd82f277492a7a084ddb56f608857
c0rp3n <ojhitchcock@gmail.com>
2020-03-18 16:13:44 +0000

    Fix terra::math::div

--- include/terra/math/div.hpp
+++ include/terra/math/div.hpp
@@ -5,9 +5,9 @@ namespace terra::math
     template<typename T>
     struct div_t
     {
-        T quot
+        T quot;
         T rem;
-    }
+    };
 
     template<typename T = size_t, typename U = size_t>
     constexpr div_t<U> div(T x, T y)
