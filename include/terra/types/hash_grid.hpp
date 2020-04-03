5249ca72941a36281da1c847960f55f2e6b6a474
c0rp3n <ojhitchcock@gmail.com>
2020-04-03 11:23:07 +0100

    [Hash Grid] Restructure and add get_cells

--- include/terra/types/hash_grid.hpp
+++ include/terra/types/hash_grid.hpp
@@ -5,27 +5,15 @@
 
 #include "../base_types.hpp"
 #include "dynarray.hpp"
+#include "rect.hpp"
 #include "vec2.hpp"
 
 namespace terra
 {
     class hash_grid
     {
-        class double_index_error : public std::exception
-        {
-            virtual const char* what() const throw()
-            {
-                return "A value already exists at this location";
-            }
-        };
-
-        tfloat cell_size;
-        size_t grid_width, grid_height;
-
-        terra::dynarray<size_t> grid;
-
     public:
-        constexpr static tfloat sqrt1_2 = 0.7071067811865476;
+        constexpr static tfloat sqrt1_2 = static_cast<tfloat>(0.7071067811865476);
         constexpr static size_t cell_empty = std::numeric_limits<size_t>::max();
 
         hash_grid();
@@ -36,6 +24,45 @@ namespace terra
 
         bool is_cell_empty(const terra::vec2& p) const;
 
+        inline tfloat get_cell_size() const;
+        inline size_t get_grid_width() const;
+        inline size_t get_grid_height() const;
+
+        inline std::vector<size_t> get_cells(const terra::rect<size_t>& rect) const;
+        std::vector<size_t> get_cells(size_t x_offset, size_t y_offset, size_t x, size_t y) const;
         std::vector<size_t> get_neighbours(const terra::vec2& p, const size_t n = 2) const;
+
+    private:
+        class double_index_error : public std::exception
+        {
+            virtual const char* what() const throw()
+            {
+                return "A value already exists at this location";
+            }
+        };
+
+        tfloat m_cell_size;
+        size_t m_grid_width, m_grid_height;
+        terra::dynarray<size_t> m_grid;
     };
 } // namespace terra
+
+tfloat terra::hash_grid::get_cell_size() const
+{
+    return this->m_cell_size;
+}
+
+size_t terra::hash_grid::get_grid_width() const
+{
+    return this->m_grid_width;
+}
+
+size_t terra::hash_grid::get_grid_height() const
+{
+    return this->m_grid_height;
+}
+
+std::vector<size_t> terra::hash_grid::get_cells(const terra::rect<size_t>& rect) const
+{
+    return this->get_cells(rect.x0, rect.y0, rect.x1, rect.y1);
+}
--- src/types/hash_grid.cpp
+++ src/types/hash_grid.cpp
@@ -2,34 +2,34 @@
 
 #include "terra/math.hpp"
 
-terra::hash_grid::hash_grid() : cell_size(0.0), grid_width(0), grid_height(0), 
-                                grid(0)
+terra::hash_grid::hash_grid() : m_cell_size(0.0), m_grid_width(0), m_grid_height(0), 
+                                m_grid(0)
 {
 }
 
 terra::hash_grid::hash_grid(size_t width, size_t height, tfloat radius) : 
-    cell_size(radius / terra::math::sqrt(2.0)),
-    grid_width(terra::math::ceil<size_t>(width / this->cell_size) + 1),
-    grid_height(terra::math::ceil<size_t>(height / this->cell_size) + 1),
-    grid(this->grid_width * this->grid_height)
+    m_cell_size(radius / terra::math::sqrt(2.0)),
+    m_grid_width(terra::math::ceil<size_t>(width / this->m_cell_size) + 1),
+    m_grid_height(terra::math::ceil<size_t>(height / this->m_cell_size) + 1),
+    m_grid(this->m_grid_width * this->m_grid_height)
 {
-    std::fill(this->grid.begin(), this->grid.end(), this->cell_empty);
+    std::fill(this->m_grid.begin(), this->m_grid.end(), this->cell_empty);
 }
 
 void terra::hash_grid::set(const terra::vec2& p, size_t index)
 {
-    const size_t x = terra::math::floor<size_t>(p.x / this->cell_size);
-    const size_t y = terra::math::floor<size_t>(p.y / this->cell_size);
+    const size_t x = terra::math::floor<size_t>(p.x / this->m_cell_size);
+    const size_t y = terra::math::floor<size_t>(p.y / this->m_cell_size);
 
-    this->grid[y * this->grid_width + x] = index;
+    this->m_grid[y * this->m_grid_width + x] = index;
 }
 
 size_t terra::hash_grid::at(const terra::vec2& p) const
 {
-    const size_t x = terra::math::floor<size_t>(p.x / this->cell_size);
-    const size_t y = terra::math::floor<size_t>(p.y / this->cell_size);
+    const size_t x = terra::math::floor<size_t>(p.x / this->m_cell_size);
+    const size_t y = terra::math::floor<size_t>(p.y / this->m_cell_size);
 
-    return this->grid[y * this->grid_width + x];
+    return this->m_grid[y * this->m_grid_width + x];
 }
 
 bool terra::hash_grid::is_cell_empty(const terra::vec2& p) const
@@ -37,26 +37,50 @@ bool terra::hash_grid::is_cell_empty(const terra::vec2& p) const
     return this->at(p) == this->cell_empty;
 }
 
-std::vector<size_t> terra::hash_grid::get_neighbours(const terra::vec2& p, size_t n) const
+std::vector<size_t> terra::hash_grid::get_cells(size_t x_offset,
+                                                size_t y_offset,
+                                                size_t width,
+                                                size_t height) const
+{
+    std::vector<size_t> cells;
+
+    for (size_t y = y_offset; y <= y_offset + height; ++y)
+    {
+        const size_t offset = y * this->m_grid_width;
+        for (size_t x = x_offset; x <= x_offset + width; ++x)
+        {
+            size_t i = this->m_grid[offset + x];
+            if (i != this->cell_empty)
+            {
+                cells.push_back(i);
+            }
+        }
+    }
+    
+    return cells;
+}
+
+std::vector<size_t> terra::hash_grid::get_neighbours(const terra::vec2& p,
+                                                     size_t n) const
 {
     std::vector<size_t> neighbours;
 
-    const size_t x_index = math::floor<size_t>(p.x / this->cell_size);
-    const size_t y_index = math::floor<size_t>(p.y / this->cell_size);
+    const size_t x_index = math::floor<size_t>(p.x / this->m_cell_size);
+    const size_t y_index = math::floor<size_t>(p.y / this->m_cell_size);
 
     const size_t min_x = std::max<size_t>(x_index - n, 0ull);
     const size_t min_y = std::max<size_t>(y_index - n, 0ull);
-    const size_t max_x = std::min<size_t>(x_index + n, this->grid_width - 1);
-    const size_t max_y = std::min<size_t>(y_index + n, this->grid_height - 1);
+    const size_t max_x = std::min<size_t>(x_index + n, this->m_grid_width - 1);
+    const size_t max_y = std::min<size_t>(y_index + n, this->m_grid_height - 1);
 
     for (size_t y = min_y; y <= max_y; ++y)
     {
-        const size_t offset = y * this->grid_width;
+        const size_t offset = y * this->m_grid_width;
         for (size_t x = min_x; x <= max_x; ++x)
         {
             if (x == x_index && y == y_index) continue;
 
-            size_t i = this->grid[offset + x];
+            size_t i = this->m_grid[offset + x];
             if (i != this->cell_empty)
             {
                 neighbours.push_back(i);
