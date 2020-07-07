5440ff19d75006367606df85f37db8a1be8f59cc
c0rp3n <ojhitchcock@gmail.com>
2020-07-07 10:49:19 +0100

    [CI] Travis make sure latest cmake is used

--- scripts/install-catch2.sh
+++ scripts/install-catch2.sh
@@ -2,5 +2,5 @@
 git clone https://github.com/catchorg/Catch2.git
 mkdir Catch2/build
 cd Catch2/build
-cmake -DBUILD_TESTING=OFF ..
+${CMAKE_DIR}/cmake -DBUILD_TESTING=OFF ..
 make && sudo make install
--- scripts/install-fastnoise-simd.sh
+++ scripts/install-fastnoise-simd.sh
@@ -2,5 +2,5 @@
 git clone https://github.com/c0rp3n/fastnoise-simd.git
 mkdir fastnoise-simd/build
 cd fastnoise-simd/build
-cmake -DFN_COMPILE_AVX2=ON -DFN_COMPILE_AVX512=OFF -DBUILD_TESTING=OFF ..
+${CMAKE_DIR}/cmake -DFN_COMPILE_AVX2=ON -DFN_COMPILE_AVX512=OFF -DBUILD_TESTING=OFF ..
 make && sudo make install
