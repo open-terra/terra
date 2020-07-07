89aeb71b489825240f7e09da2518506d847e04aa
c0rp3n <ojhitchcock@gmail.com>
2020-07-07 10:49:19 +0100

    Revert "[CI] Travis make sure latest cmake is used"
    This reverts commit 5440ff19d75006367606df85f37db8a1be8f59cc.

--- scripts/install-fastnoise-simd.sh
+++ scripts/install-fastnoise-simd.sh
@@ -2,5 +2,5 @@
 git clone https://github.com/c0rp3n/fastnoise-simd.git
 mkdir fastnoise-simd/build
 cd fastnoise-simd/build
-${CMAKE_DIR}/cmake -DFN_COMPILE_AVX2=ON -DFN_COMPILE_AVX512=OFF -DBUILD_TESTING=OFF ..
+cmake -DFN_COMPILE_AVX2=ON -DFN_COMPILE_AVX512=OFF -DBUILD_TESTING=OFF ..
 make && sudo make install
