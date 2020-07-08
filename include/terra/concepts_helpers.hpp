a9a4355acd54e0287d9e2bd5ac5bbebe795f48e1
c0rp3n <ojhitchcock@gmail.com>
2020-07-08 12:27:22 +0100

    [Concepts] Fix span creation in `to_span`
    Added a constant cast when creating the span so that if `T` is not a constant type the span doesnt treat the data as const.
    This was happening because `std::data` was returning `const T*` instead of `T*`.

--- include/terra/concepts_helpers.hpp
+++ include/terra/concepts_helpers.hpp
@@ -6,15 +6,21 @@
 
 namespace terra
 {
-    template<class T, class Array> requires terra::Container<Array, T>
-    constexpr auto to_span(Array& a) -> std::span<T, std::dynamic_extent>
+    template<class T, class Array>
+    requires terra::Container<Array, T>
+    constexpr auto to_span(const Array& a) -> std::span<T>
     {
-        return std::span(std::data(a), std::size(a));
-    }
+        /*
+        if constexpr (std::is_const<T>::value)
+        {
+            return std::span(std::data(a), std::size(a));
+        }
+        else
+        {
+            return std::span(const_cast<T*>(std::data(a)), std::size(a));
+        }
+        */
 
-    template<class T, class Array> requires terra::Container<Array, T>
-    constexpr auto to_span(const Array& a) -> std::span<const T, std::dynamic_extent>
-    {
-        return std::span(std::data(a), std::size(a));
+        return std::span(const_cast<T*>(std::data(a)), std::size(a));
     }
 } // namespace terra
